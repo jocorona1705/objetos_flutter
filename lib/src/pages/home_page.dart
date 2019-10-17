175ec1254c3c0cabbdc258024be266f3b96de2bf
unknown <Administrator@D232IT018.corp.joysonsafety.com>
2019-10-17 09:58:10 -0500

    terminado future builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -19,27 +19,29 @@ class HomePage extends StatelessWidget {
     return FutureBuilder(
       future: providerData.cargarData(),
       initialData: [],
+      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ){
+        return ListView(
+            children: _listaItems( snapshot.data ),
+        );
+      },
     );
-    
-    // return ListView(
-    //   children: _listaItems(),
-    // );
   }
 
-  List<Widget>_listaItems() {
-    return [
-      ListTile(
-        title: Text('Texto'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Texto'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Texto'),
-      ),
-      Divider()
-    ];
+  List<Widget>_listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach( ( f ) {
+      final widgetTemp = ListTile(
+        title: Text( f['texto'] ),
+        leading: Icon( Icons.account_circle ),
+        trailing: Icon( Icons.arrow_forward_ios ),
+        onTap: () {
+
+        },
+      );
+      opciones..add(widgetTemp)
+              ..add(Divider());
+    });
+    return opciones;
   }
 }
