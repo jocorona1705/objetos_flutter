025b61a0d7312473a40a82fbcef472d9d9db540b
unknown <Administrator@D232IT018.corp.joysonsafety.com>
2019-10-17 16:24:22 -0500

    asdasd

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:objetos_flutter/src/pages/alert_page.dart';
 import 'package:objetos_flutter/src/pages/avatar_page.dart';
+import 'package:objetos_flutter/src/pages/card_page.dart';
 import 'package:objetos_flutter/src/pages/home_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
     return <String, WidgetBuilder>{
-      '/'         : ( BuildContext context) => HomePage(),
-      'alert'     : ( BuildContext context) => AlertPage(),
-      'avatar'    : ( BuildContext context) => AvatarPage(),
+      '/'         : ( BuildContext context ) => HomePage(),
+      'alert'     : ( BuildContext context ) => AlertPage(),
+      'avatar'    : ( BuildContext context ) => AvatarPage(),
+      'card'      : ( BuildContext context ) => CardPage(),
   };
 }
 
